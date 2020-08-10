608b63832329fd3a6c4154b392f04ea0674cb1bf
Ken Leiserson <kenl09@gmail.com>
2020-08-10 16:50:52 -0700

    finish updating shared lib

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,66 +1,136 @@
-// (Lines like the one below ignore selected Clippy rules
-//  - it's useful when you want to check your code with `cargo make verify`
-// but some rules are too "annoying" or are not applicable for your case.)
-#![allow(clippy::wildcard_imports)]
-
 use seed::{prelude::*, *};
-
-// ------ ------
-//     Init
-// ------ ------
-
-// `init` describes what should happen when your app started.
-fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
-    Model::default()
-}
-
-// ------ ------
-//     Model
-// ------ ------
+use serde::{ Deserialize, Serialize };
+use serde_json::Value;
+use std::future::Future;
+use seed::browser::fetch::header::Header;
+use seed::virtual_dom::el_ref::el_ref;
+use shared::responses::{ApiResponse, TokenResponse, UserResponse};
+use shared::payloads::CreateUserPayload;
+use web_sys::HtmlInputElement;
 
 // `Model` describes our app state.
-type Model = i32;
-
-// ------ ------
-//    Update
-// ------ ------
-
-// (Remove the line below once any of your `Msg` variants doesn't implement `Copy`.)
-#[derive(Copy, Clone)]
-// `Msg` describes the different events you can modify state with.
-enum Msg {
-    Increment,
+#[derive(Debug)]
+pub struct Model {
+    username_input: ElRef<HtmlInputElement>,
+    password_input: ElRef<HtmlInputElement>,
+    auth_token: Option<String>,
+    me: Option<UserResponse>,
 }
 
-// `update` describes how to handle each `Msg`.
-fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
-    match msg {
-        Msg::Increment => *model += 1,
+impl Default for Model {
+    fn default() -> Self {
+        Self { 
+            username_input: ElRef::default(),
+            password_input: ElRef::default(),
+            auth_token: None,
+            me: None,
+         }
     }
 }
 
-// ------ ------
-//     View
-// ------ ------
+// 'Msg" describes events you modify state with
+#[derive(Clone)]
+enum Msg {
+    CreateUserFormSubmitted,
+    Authenticated(String),
+    MeLoaded(UserResponse),
+    #[allow(dead_code)]
+    Noop,
+}
 
-// (Remove the line below once your `Model` become more complex.)
-#[allow(clippy::trivially_copy_pass_by_ref)]
+// `update` describes how to handle each `Msg`.
+fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::Noop => {},
+        Msg::Authenticated(token) => {
+            model.auth_token = Some(token);
+
+            let auth_token = model.auth_token.clone().unwrap();
+            orders.perform_cmd(async move {
+                let req = Request::new("http://localhost:8080/me")
+                    .header(Header::bearer(auth_token))
+                    .method(Method::Get);
+                let resp = fetch(req).await.unwrap();
+
+                let user = resp
+                    .check_status() // ensure we've got 2xx status
+                    .expect("status check failed")
+                    .json::<Data<UserResponse>>()
+                    .await
+                    .expect("deserialization failed")
+                    .data;
+
+                Msg::MeLoaded(user);
+            });
+        },
+        Msg::MeLoaded(user) => {
+            model.me = Some(user);
+            log!(model);
+        },
+        Msg::CreateUserFormSubmitted => {
+            let username = model.username_input.get().unwrap().value();
+            let password = model.password_input.get().unwrap().value();
+            
+            orders.perform_cmd(async {
+                let form = CreateUserPayload { username, password };
+                let req = Request::new("http://localhost:8080/users").method(Method::Post)
+                    .json(&form)
+                    .unwrap();
+                let resp = fetch(req).await.unwrap();
+
+                let token = resp
+                    .check_status() // ensure we've got 2xx status
+                    .expect("status check failed")
+                    .json::<Data<TokenResponse>>()
+                    .await
+                    .expect("deserialization failed")
+                    .data
+                    .token;
+
+                Msg::Authenticated(token)
+
+            });
+        }
+    }
+}
+
+
+#[derive(Debug, Deserialize)]
+struct Data<T> {
+    data: T,
+}
+
 // `view` describes what to display.
 fn view(model: &Model) -> Node<Msg> {
     div![
-        "Hello. This is a counter: ",
-        C!["counter"],
-        button![model, ev(Ev::Click, |_| Msg::Increment),],
+        div![
+        input![
+            el_ref(&model.username_input),
+            attrs! { At::Placeholder => "Username"},
+            ]
+        ],
+    div![
+        input![
+            el_ref(&model.password_input),
+            attrs! { At::Placeholder => "Password"},
+            ]
+        ],
+        div![
+            button![
+                "Submit",
+                ev(Ev::Click, |_| Msg::CreateUserFormSubmitted),
+            ]
+        ]
     ]
 }
 
-// ------ ------
-//     Start
-// ------ ------
+// `init` describes what should happen when your app started.
+fn init(url: Url, _: &mut impl Orders<Msg>) -> Model {
+    Model::default()
+}
 
 // (This function is invoked by `init` function in `index.html`.)
 #[wasm_bindgen(start)]
 pub fn start() {
-    // Mount the `app` to the element with the `id` "app".
     App::start("app", init, update, view);
 }
